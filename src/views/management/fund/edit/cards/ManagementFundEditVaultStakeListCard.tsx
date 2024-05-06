d192b1757111724e52134d5e50bc9ebab047fc85
PSheon <pauljiang61020@gmail.com>
2024-05-06 12:32:56 +0800

    feat: updated management fund edit vault layout.

---
+++ src/views/management/fund/edit/cards/ManagementFundEditVaultStakeListCard.tsx
@@ -0,0 +1,221 @@
+// ** React Imports
+import { useState } from 'react'
+
+// ** Next Imports
+import Link from 'next/link'
+
+// ** MUI Imports
+import { styled } from '@mui/material/styles'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import Typography from '@mui/material/Typography'
+
+// ** Third-Party Imports
+import { format } from 'date-fns'
+
+// ** Custom Component Imports
+import CustomChip from 'src/@core/components/mui/chip'
+import DataGrid, { GridColDef } from 'src/views/shared/wrapped-data-grid'
+
+// ** API Imports
+import { useFindMeQuery } from 'src/store/api/management/activityLog'
+
+// ** Util Imports
+import { getActivityLogStatusProperties, getActivityLogActionProperties, getActivityLogRefContentLink } from 'src/utils'
+
+// ** Type Imports
+import { ActivityLogType } from 'src/types/api/activityLogTypes'
+
+interface CellType {
+  row: ActivityLogType
+}
+
+// ** Styled Components
+const LinkStyled = styled(Link)(({ theme }) => ({
+  fontWeight: 600,
+  fontSize: '1rem',
+  cursor: 'pointer',
+  textDecoration: 'none',
+  color: theme.palette.text.secondary,
+  '&:hover': {
+    color: theme.palette.primary.main
+  }
+}))
+
+const ManagementFundEditVaultStakeListCard = () => {
+  // ** States
+  const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 })
+
+  // ** Hooks
+  const { data: activitiesData, isLoading: isActivityLogsLoading } = useFindMeQuery({
+    filters: {},
+    sort: ['date:desc'],
+    pagination: {
+      page: paginationModel.page + 1,
+      pageSize: paginationModel.pageSize
+    }
+  })
+
+  // ** Vars
+  const activityLogs = activitiesData?.data || []
+  const totalRows = activitiesData?.meta.pagination.total || 0
+  const columns: GridColDef[] = [
+    {
+      flex: 1,
+      minWidth: 80,
+      field: 'tokenId',
+      headerName: '編號',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 600, color: 'text.secondary' }}>
+          # {row.id}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 80,
+      field: 'cover',
+      headerName: '卡面',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 600, color: 'text.secondary' }}>
+          # {row.id}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 80,
+      field: 'package',
+      headerName: '方案',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 600, color: 'text.secondary' }}>
+          # {row.id}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 80,
+      field: 'value',
+      headerName: '額度',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 600, color: 'text.secondary' }}>
+          # {row.id}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 80,
+      field: 'interest',
+      headerName: '已產生利潤',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 600, color: 'text.secondary' }}>
+          # {row.id}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 80,
+      field: 'ownedAddress',
+      headerName: '持有位址',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 600, color: 'text.secondary' }}>
+          # {row.id}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      minWidth: 80,
+      field: 'unlockDateTime',
+      headerName: '解鎖時間',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 600, color: 'text.secondary' }}>
+          # {row.id}
+        </Typography>
+      )
+    },
+    {
+      flex: 1,
+      field: 'status',
+      minWidth: 110,
+      headerName: '狀態',
+      renderCell: ({ row }: CellType) => {
+        const { color, title } = getActivityLogStatusProperties(row.status)
+
+        return (
+          <CustomChip
+            skin='light'
+            size='small'
+            rounded
+            label={title}
+            color={color}
+            sx={{ textTransform: 'capitalize', '& .MuiChip-label': { lineHeight: '18px' } }}
+          />
+        )
+      }
+    },
+    {
+      flex: 2,
+      minWidth: 80,
+      field: 'action',
+      headerName: '操作',
+      renderCell: ({ row }: CellType) => {
+        const { color, title } = getActivityLogActionProperties(row.action)
+
+        return (
+          <Typography noWrap sx={{ fontWeight: 600, color }}>
+            {`@${title}`}
+          </Typography>
+        )
+      }
+    },
+    {
+      flex: 3,
+      minWidth: 160,
+      field: 'refContentType',
+      headerName: '執行個體',
+      renderCell: ({ row }: CellType) => {
+        const link = getActivityLogRefContentLink(row)
+
+        return <LinkStyled href={link}>{`${row.refContentType}#${row.id}`}</LinkStyled>
+      }
+    },
+    {
+      flex: 3,
+      minWidth: 250,
+      field: 'date',
+      headerName: '日期',
+      renderCell: ({ row }: CellType) => (
+        <Typography noWrap sx={{ fontWeight: 600, color: 'text.secondary' }}>
+          {format(new Date(row.date), 'PPpp')}
+        </Typography>
+      ),
+      valueGetter: ({ row }: CellType) => format(new Date(row.date), 'PPpp')
+    }
+  ]
+
+  return (
+    <Card>
+      <CardHeader title='Staked List' />
+      <DataGrid
+        autoHeight
+        loading={isActivityLogsLoading}
+        rows={activityLogs}
+        columns={columns}
+        checkboxSelection
+        disableRowSelectionOnClick
+        pageSizeOptions={[10, 25, 50]}
+        paginationMode='server'
+        paginationModel={paginationModel}
+        onPaginationModelChange={setPaginationModel}
+        rowCount={totalRows}
+        sx={{ '& .MuiDataGrid-columnHeaders': { borderRadius: 0 } }}
+      />
+    </Card>
+  )
+}
+
+export default ManagementFundEditVaultStakeListCard
--- src/views/management/fund/edit/tabs/ManagementFundEditVaultTabPanel.tsx
+++ src/views/management/fund/edit/tabs/ManagementFundEditVaultTabPanel.tsx
@@ -2,6 +2,12 @@
 import Grid from '@mui/material/Grid'
 import TabPanel from '@mui/lab/TabPanel'
 
+// ** Custom Component Imports
+import ManagementFundEditVaultProtocolCard from 'src/views/management/fund/edit/cards/ManagementFundEditVaultProtocolCard'
+import ManagementFundEditVaultStatementCard from 'src/views/management/fund/edit/cards/ManagementFundEditVaultStatementCard'
+import ManagementFundEditVaultStatisticsCard from 'src/views/management/fund/edit/cards/ManagementFundEditVaultStatisticsCard'
+import ManagementFundEditVaultStakeListCard from 'src/views/management/fund/edit/cards/ManagementFundEditVaultStakeListCard'
+
 // ** Type Imports
 import { FundType } from 'src/types/api/fundTypes'
 
@@ -11,15 +17,30 @@ interface Props {
 
 const ManagementFundEditVaultTabPanel = (props: Props) => {
   // ** Props
-  const {
-    /* initFundEntity */
-  } = props
+  const { initFundEntity } = props
 
   return (
     <TabPanel sx={{ p: 0 }} value='vault'>
       <Grid container spacing={6}>
-        <Grid item xs={12}>
-          456
+        <Grid item xs={12} md={5} lg={4}>
+          <Grid container spacing={6}>
+            <Grid item xs={12}>
+              <ManagementFundEditVaultProtocolCard initFundEntity={initFundEntity} />
+            </Grid>
+            <Grid item xs={12}>
+              <ManagementFundEditVaultStatementCard />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={7} lg={8}>
+          <Grid container spacing={6}>
+            <Grid item xs={12}>
+              <ManagementFundEditVaultStatisticsCard />
+            </Grid>
+            <Grid item xs={12}>
+              <ManagementFundEditVaultStakeListCard />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </TabPanel>
