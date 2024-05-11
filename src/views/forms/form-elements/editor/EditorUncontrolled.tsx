3a3bd41061d9eb07e24ba9b262939a231880972d
PSheon <pauljiang61020@gmail.com>
2024-05-11 09:08:19 +0800

    feat: removed redundant views.

--- src/views/forms/form-elements/editor/EditorUncontrolled.tsx
+++
@@ -1,6 +0,0 @@
-// ** Core Component Imports
-import ReactDraftWysiwyg from 'src/@core/components/react-draft-wysiwyg'
-
-const EditorUncontrolled = () => <ReactDraftWysiwyg />
-
-export default EditorUncontrolled
