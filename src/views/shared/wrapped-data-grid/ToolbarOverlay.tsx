807b64a42072ba61132a8f9f23d24dda6a01d834
PSheon <pauljiang61020@gmail.com>
2024-05-11 13:33:57 +0800

    feat: updated shared data-grid component.

--- src/views/shared/wrapped-data-grid/ToolbarOverlay.tsx
+++ src/views/shared/wrapped-data-grid/ToolbarOverlay.tsx
@@ -4,13 +4,13 @@ import Typography from '@mui/material/Typography'
 import Collapse from '@mui/material/Collapse'
 import { GridToolbarExport, useGridApiContext, useGridSelector, gridRowSelectionStateSelector } from '@mui/x-data-grid'
 
-const ExportToolbar = () => {
+const ToolbarOverlay = () => {
   // ** Hooks
   const apiRef = useGridApiContext()
   const selectedRows = useGridSelector(apiRef, gridRowSelectionStateSelector)
 
   return (
-    <Collapse in={selectedRows.length > 0}>
+    <Collapse in={selectedRows.length > 0} mountOnEnter unmountOnExit>
       <Stack
         spacing={6}
         direction='row'
@@ -18,11 +18,11 @@ const ExportToolbar = () => {
         alignItems='center'
         sx={{ p: theme => theme.spacing(2, 5) }}
       >
-        <Typography variant='subtitle2'>{`已選取 ${selectedRows.length} 個`}</Typography>
+        <Typography variant='subtitle2'>{`${selectedRows.length} selected`}</Typography>
         <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
       </Stack>
     </Collapse>
   )
 }
 
-export default ExportToolbar
+export default ToolbarOverlay
