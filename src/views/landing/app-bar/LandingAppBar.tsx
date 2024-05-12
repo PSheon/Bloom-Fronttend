f647dae9f8f74db3c30aea93e2b39a11f1db1432
PSheon <pauljiang61020@gmail.com>
2024-05-12 19:11:52 +0800

    feat: updated landing app bar layout.

--- src/views/landing/app-bar/LandingAppBar.tsx
+++ src/views/landing/app-bar/LandingAppBar.tsx
@@ -1,23 +1,42 @@
+// ** React Imports
+import { useState } from 'react'
+
 // ** Next Imports
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 // ** MUI Imports
 import { styled, useTheme, lighten } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import useScrollTrigger from '@mui/material/useScrollTrigger'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
+import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
+import IconButton from '@mui/material/IconButton'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import Badge from '@mui/material/Badge'
 import Avatar from '@mui/material/Avatar'
+import SwipeableDrawer from '@mui/material/SwipeableDrawer'
+import Divider from '@mui/material/Divider'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
 
 // ** Third-Party Components
 import { useSession } from 'next-auth/react'
 
 // ** Custom Component Imports
 import LogoImage from 'src/views/shared/LogoImage'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Hook Imports
+import { useSettings } from 'src/@core/hooks/useSettings'
 
 // ** Util Imports
 import { hexToRGBA } from 'src/@core/utils/hex-to-rgba'
@@ -29,6 +48,8 @@ import themeConfig from 'src/configs/themeConfig'
 // ** Type Imports
 import type { AppBarProps } from '@mui/material/AppBar'
 import type { ToolbarProps } from '@mui/material/Toolbar'
+import type { Settings } from 'src/@core/context/settingsContext'
+import type { Mode } from 'src/@core/layouts/types'
 
 const StyledAppBar = styled(AppBar)<AppBarProps>(({ theme }) => ({
   transition: 'padding .25s ease-in-out',
@@ -72,15 +93,32 @@ const BadgeContentSpan = styled('span')(({ theme }) => ({
 }))
 
 const LandingAppBar = () => {
+  // ** States
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false)
+
   // ** Hooks
   const theme = useTheme()
   const scrollTrigger = useScrollTrigger({ threshold: 0, disableHysteresis: true })
+  const hidden = useMediaQuery(theme.breakpoints.down('lg'))
+  const { settings, saveSettings } = useSettings()
   const router = useRouter()
   const session = useSession()
 
   // ** Logics
   const handleDirectToDashboard = () => {
     router.push('/')
+  }
+
+  const handleOpenNav = () => {
+    setIsNavOpen(true)
+  }
+
+  const handleCloseNav = () => {
+    setIsNavOpen(false)
+  }
+
+  const handleChangeSettings = (field: keyof Settings, value: Settings[keyof Settings]): void => {
+    saveSettings({ ...settings, [field]: value })
   }
 
   return (
@@ -118,20 +156,35 @@ const LandingAppBar = () => {
         }}
       >
         <Stack direction='row' alignItems='center' justifyContent='space-between' sx={{ width: '100%' }}>
-          <Stack direction='row' spacing={6} alignItems='center' className='actions-left'>
-            <Stack direction='row' spacing={2} alignItems='center' className='actions-left'>
-              <Link href='/'>
-                <Stack alignItems='center' justifyContent='center'>
-                  <LogoImage width={40} height={40} />
-                </Stack>
-              </Link>
-              <Typography variant='subtitle1' component='span' sx={{ fontWeight: 600 }}>
-                {themeConfig.templateName}
-              </Typography>
-            </Stack>
-            <Stack direction='row' spacing={2} alignItems='center' className='actions-left'>
-              <StyledLink href='/article'>Article</StyledLink>
-            </Stack>
+          <Stack direction='row' spacing={4} alignItems='center' className='actions-left'>
+            {hidden ? (
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <IconButton color='inherit' sx={{ ml: -2.75 }} onClick={handleOpenNav}>
+                  <Icon icon='mdi:menu' />
+                </IconButton>
+                <Link href='/'>
+                  <Stack alignItems='center' justifyContent='center'>
+                    <LogoImage width={40} height={40} />
+                  </Stack>
+                </Link>
+              </Box>
+            ) : (
+              <Stack direction='row' spacing={2} alignItems='center'>
+                <Link href='/'>
+                  <Stack alignItems='center' justifyContent='center'>
+                    <LogoImage width={40} height={40} />
+                  </Stack>
+                </Link>
+                <Typography variant='subtitle1' component='span' sx={{ ml: 2, fontWeight: 600 }}>
+                  {themeConfig.templateName}
+                </Typography>
+              </Stack>
+            )}
+            {!hidden ? (
+              <Stack direction='row' spacing={2} alignItems='center' className='actions-left'>
+                <StyledLink href='/article'>Article</StyledLink>
+              </Stack>
+            ) : null}
           </Stack>
           <Stack direction='row' spacing={2} alignItems='center' className='actions-right'>
             {session.status === 'authenticated' ? (
@@ -165,6 +218,89 @@ const LandingAppBar = () => {
           </Stack>
         </Stack>
       </StyledToolbar>
+
+      <SwipeableDrawer
+        anchor='left'
+        open={isNavOpen}
+        onOpen={handleOpenNav}
+        onClose={handleCloseNav}
+        PaperProps={{
+          sx: {
+            width: 240,
+            height: theme => `calc(100% - 2 * ${theme.spacing(4)})`,
+            borderRadius: '10px',
+            m: 4,
+            p: 4
+          }
+        }}
+      >
+        <Stack direction='row' spacing={2} alignItems='center'>
+          <Link href='/'>
+            <Stack alignItems='center' justifyContent='center'>
+              <LogoImage width={40} height={40} />
+            </Stack>
+          </Link>
+          <Typography variant='subtitle1' component='span' sx={{ ml: 2, fontWeight: 600 }}>
+            {themeConfig.templateName}
+          </Typography>
+        </Stack>
+        <Box>
+          <Divider sx={{ my: theme => `${theme.spacing(4)} !important` }} />
+        </Box>
+        <Box>
+          <Typography variant='caption' component='span' sx={{ fontWeight: 600 }}>
+            Pages
+          </Typography>
+        </Box>
+        <List disablePadding>
+          <ListItem disablePadding>
+            <ListItemButton component={Link} href='/article/list' sx={{ px: 0 }}>
+              <ListItemIcon>
+                <Icon icon='mdi:file-document-outline' />
+              </ListItemIcon>
+              <ListItemText
+                primary={
+                  <Typography variant='subtitle1' component='span' sx={{ fontWeight: 600 }}>
+                    Article
+                  </Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
+        </List>
+
+        <List disablePadding sx={{ mt: 'auto' }}>
+          <ListItem disablePadding>
+            {settings.mode === 'dark' ? (
+              <ListItemButton sx={{ px: 0 }} onClick={() => handleChangeSettings('mode', 'light' as Mode)}>
+                <ListItemIcon>
+                  <Icon icon='mdi:moon-and-stars' />
+                </ListItemIcon>
+                <ListItemText
+                  primary={
+                    <Typography variant='body1' component='span'>
+                      Dark
+                    </Typography>
+                  }
+                />
+              </ListItemButton>
+            ) : (
+              <ListItemButton sx={{ px: 0 }} onClick={() => handleChangeSettings('mode', 'dark' as Mode)}>
+                <ListItemIcon>
+                  <Icon icon='mdi:white-balance-sunny' />
+                </ListItemIcon>
+                <ListItemText
+                  primary={
+                    <Typography variant='body1' component='span'>
+                      Light
+                    </Typography>
+                  }
+                />
+              </ListItemButton>
+            )}
+          </ListItem>
+        </List>
+      </SwipeableDrawer>
     </StyledAppBar>
   )
 }
