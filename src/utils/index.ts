99afa5648d9a0ddceb223e8956273d9b1d6d3560
PSheon <pauljiang61020@gmail.com>
2024-05-12 12:50:58 +0800

    feat: added article utils.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from 'src/utils/acl'
 export * from 'src/utils/ethereum'
 export * from 'src/utils/blog'
+export * from 'src/utils/article'
 export * from 'src/utils/announcement'
 export * from 'src/utils/fund'
 export * from 'src/utils/package'
