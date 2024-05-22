1b1dd3a1de346318c16ad0729c7ef6fea618cbae
PSheon <pauljiang61020@gmail.com>
2024-05-22 17:22:41 +0800

    feat: updated portfolio layout.

---
+++ src/views/portfolio/grids/PortfolioMePositionGrid.tsx
@@ -0,0 +1,20 @@
+// ** MUI Imports
+import Grid from '@mui/material/Grid'
+
+// ** Custom Component Imports
+import PortfolioOwnedSFTSkeletonCard from 'src/views/portfolio/cards/me-positions/PortfolioOwnedSFTSkeletonCard'
+
+// TODO: Fill here later
+const PortfolioMePositionGrid = () => {
+  return (
+    <Grid container spacing={6} className='match-height'>
+      {[...Array(3).keys()].map(index => (
+        <Grid key={`portfolio-position-skeleton-${index}`} item xs={12} sm={6} md={4}>
+          <PortfolioOwnedSFTSkeletonCard />
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+export default PortfolioMePositionGrid
